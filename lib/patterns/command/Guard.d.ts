277b4f6e5874801d6d793453e5ba51d5b6cf7374
RollinSafary <rollinsafary@hotmail.com>
2021-05-22 12:32:48 +0400

    modules update, tsconfig and webpack config update

--- lib/patterns/command/Guard.d.ts
+++
@@ -1,4 +0,0 @@
-import Notifier from '../observer/Notifier';
-export default abstract class Guard extends Notifier {
-    abstract approve(notificationName?: string, ...args: any[]): boolean | Promise<boolean>;
-}
--- lib/patterns/observer/Notifier.d.ts
+++
@@ -1,8 +0,0 @@
-import Facade from '../facade/Facade';
-export default class Notifier {
-    protected facade: Facade;
-    protected multitonKey: string;
-    initializeNotifier(key: string): void;
-    protected sendNotification(notificationName: string, ...args: any[]): void;
-    private getFacade;
-}
